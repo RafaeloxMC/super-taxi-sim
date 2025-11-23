f6afb336f29b7293bb9db12852f7a7496e75551b
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-11-23 21:44:35 +0100

    Update scene manager to be a scene

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -1,4 +1,10 @@
 extends Node
 
-func call_scene(scene: PackedScene) -> void:
+@export var scenes: Dictionary[String, PackedScene]
+
+func call_packed(scene: PackedScene) -> void:
 	get_tree().change_scene_to_packed(scene)
+
+func call_scene(scene: String) -> void:
+	if scenes.get(scene):
+		call_packed(scenes.get(scene))
