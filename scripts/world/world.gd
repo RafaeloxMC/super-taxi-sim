a36233bf7071a3d658bf960e379eb47e77a75733
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-07 14:36:14 +0100

    Add darker sky at night

--- scripts/world/world.gd
+++ scripts/world/world.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 @export var lighting_source: DirectionalLight3D
+@export var world_environment: WorldEnvironment
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var brightness = GameManager.daytime_to_brightness(GameManager.time)
 	lighting_source.light_energy = brightness
+	(world_environment.environment.sky.sky_material as PanoramaSkyMaterial).energy_multiplier = brightness
