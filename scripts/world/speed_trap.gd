e98360899af9c40fdbb18b222cd1331318e5cd99
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-13 22:07:33 +0100

    Adjust speed trap to new speed sign mechanic

--- scripts/world/speed_trap.gd
+++ scripts/world/speed_trap.gd
@@ -1,6 +1,5 @@
 extends StaticBody3D
 
-@export var speed_limit: int = 50
 @onready var flash: SpotLight3D = $speed_trap/SpotLight3D
 
 var timeout: float = 0.05
@@ -18,8 +17,8 @@ func _process(_delta: float) -> void:
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group(GameManager.taxi_group):
-		if GameManager.speed > speed_limit:
-			GameManager.speed_trap_triggered.emit(GameManager.speed, float(speed_limit))
+		if GameManager.speed > GameManager.speed_limit:
+			GameManager.speed_trap_triggered.emit(GameManager.speed, float(GameManager.speed_limit))
 			flash.light_energy = light_strength
 			await get_tree().create_timer(timeout).timeout
 			flash.light_energy = 0
