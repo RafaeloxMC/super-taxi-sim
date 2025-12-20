c9f38a6ae63b886c1adadcdf0b1221fd38e74836
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-20 16:18:23 +0100

    Fix errors in character zone

--- scripts/world/character.gd
+++ scripts/world/character.gd
@@ -3,7 +3,7 @@ extends Area3D
 var taxi_body: Node3D
 
 func _process(_delta: float) -> void:
-	if self.overlaps_body(taxi_body) and GameManager.speed == 0:
+	if taxi_body && self.overlaps_body(taxi_body) and GameManager.speed == 0:
 		GameManager.customer = GameManager.generate_random_name()
 		print("New customer! " + str(GameManager.customer))
 		self.queue_free()
