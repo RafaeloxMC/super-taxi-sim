be0201dc89c445433a2b0274b588be7ffa66ae33
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-20 09:24:46 +0100

    Modify speed signs to only work one-way

--- scripts/world/speed_sign.gd
+++ scripts/world/speed_sign.gd
@@ -1,5 +1,5 @@
 extends Area3D
-
+@onready var direction_area: Area3D = $DirectionArea
 @onready var speed_limit_label: Label3D = $StaticBody3D/Model/SpeedLimit
 
 @export var speed_limit: int = 40
@@ -8,5 +8,6 @@ func _ready() -> void:
 	speed_limit_label.text = str(speed_limit)
 
 func _on_body_entered(body: Node3D) -> void:
-	if body.is_in_group(GameManager.taxi_group):
+	if body.is_in_group(GameManager.taxi_group) && direction_area.overlaps_body(body):
 		GameManager.speed_limit = speed_limit
+		print("Changed speed limit to " + str(speed_limit) + " km/h")
