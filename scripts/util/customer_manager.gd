4024d4cd349f69aa8e4c5d87bdd451568803830c
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-29 09:11:39 +0100

    Improve codebase of customer manager

--- scripts/util/customer_manager.gd
+++ scripts/util/customer_manager.gd
@@ -9,14 +9,17 @@ var current_customer_dropoff_location: Vector3
 
 func spawn_customer() -> void:
 	var road = pick_random_road()
+	if road == null:
+		return
 	var pickup_char = pickup_character.instantiate() as Node3D
 	pickup_char.position.y = 2
 	pickup_char.position.x = 6 if randi_range(0, 1) == 1 else -6
 	road.add_child(pickup_char)
-	pass
 
 func spawn_dropoff(customer: String) -> void:
 	var road = pick_random_road()
+	if road == null:
+		return
 	var dropoff_loc = dropoff_location.instantiate() as Node3D
 	dropoff_loc.customer = customer
 	dropoff_loc.tick()
@@ -24,7 +27,6 @@ func spawn_dropoff(customer: String) -> void:
 	dropoff_loc.position.x = 6 if randi_range(0, 1) == 1 else -6
 	road.add_child(dropoff_loc)
 	self.current_customer_dropoff_location = dropoff_loc.global_position
-	pass
 
 func pick_random_road() -> Node3D:
 	var roads_container = get_tree().get_first_node_in_group(roads_parent_group)
@@ -32,9 +34,7 @@ func pick_random_road() -> Node3D:
 		print("Could not spawn customer: Roads Parent not found!")
 		return
 	var children = roads_container.get_children()
-	for child in children:
-		if child.has_node("Character"):
-			children.erase(child)
+	children = children.filter(func(c): return !c.has_node("Character"))
 	if children.size() == 0:
 		print("No empty roads left, returning null.")
 		return null
