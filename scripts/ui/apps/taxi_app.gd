b593d9d017328c3f2b1212d07a706fbee4f34950
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-30 23:17:52 +0100

    Update map & add parking lot

--- scripts/ui/apps/taxi_app.gd
+++ scripts/ui/apps/taxi_app.gd
@@ -10,5 +10,5 @@ func _ready() -> void:
 	cname.text = GameManager.customer if GameManager.customer != "" else "None"
 	sub_viewport.world_3d = get_viewport().world_3d
 	camera_3d.current = true
-	camera_3d.position = Vector3(0, 150, 0)
+	camera_3d.position = Vector3(75, 150, 15)
 	camera_3d.rotation_degrees = Vector3(-90, 0, 0)
