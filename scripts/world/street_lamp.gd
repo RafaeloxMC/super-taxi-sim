72a9e273717ec9743d5273078f8391b8664348bc
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-31 23:00:57 +0100

    Add a lot of lamps

--- scripts/world/street_lamp.gd
+++ scripts/world/street_lamp.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var spot_light_3d: SpotLight3D = $Model/SpotLight3D
+@onready var spot_light_3d: OmniLight3D = $Model/SpotLight3D
 
 func _process(_delta: float) -> void:
 	spot_light_3d.visible = false if GameManager.time >= 30000 && GameManager.time <= 60000 else true
