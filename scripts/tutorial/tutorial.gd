c89d45c5425a3287bb6a7c8f73b273a047860a97
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-01-03 12:17:42 +0100

    Add dialog manager

--- scripts/tutorial/tutorial.gd
+++ scripts/tutorial/tutorial.gd
@@ -5,6 +5,7 @@ extends Node3D
 func _ready() -> void:
 	CustomerManager.customer_dropped_off.connect(_dropped_off)
 	CustomerManager.customer_picked_up.connect(_picked_up)
+	CustomerManager.spawn_customer()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,4 +16,5 @@ func _dropped_off(name: String, location: Vector3) -> void:
 	pass
 	
 func _picked_up(name: String, location: Vector3) -> void:
-	pass
+	var dialog = "Woah, " + name + ", that's our first customer! Let's bring them to the place they want to go to!"
+	DialogManager.call_dialog(name, dialog)
