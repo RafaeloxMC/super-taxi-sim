d355071844193d36ea9c1b23fe4cba92bf031777
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-01-04 14:58:30 +0100

    Add initial message to tutorial

--- scripts/tutorial/tutorial.gd
+++ scripts/tutorial/tutorial.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	CustomerManager.customer_dropped_off.connect(_dropped_off)
 	CustomerManager.customer_picked_up.connect(_picked_up)
 	CustomerManager.spawn_customer()
+	DialogManager.call_dialog("Shiba", "Hey, thank you for working at my taxi central! You can find your first customer in the taxi app on the phone! Press " + InputMap.action_get_events("phone").get(0).as_text().split(" ")[0] + " to open the phone!")
 
 func _dropped_off(_customer_name: String, _location: Vector3) -> void:
 	var dialog = "Yo, that was amazing! That's how we do it! Maybe you can be a little faster next time and even get a bonus from the customer!"
