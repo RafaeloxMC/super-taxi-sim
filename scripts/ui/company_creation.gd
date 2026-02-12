16b01c1e961d949c900beda55b6deca1186da26b
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-02-12 14:01:25 +0100

    Send player to tutorial instead of open world

--- scripts/ui/company_creation.gd
+++ scripts/ui/company_creation.gd
@@ -7,4 +7,4 @@ func _on_line_edit_text_changed(new_text: String) -> void:
 	GameManager.company_name = new_text
 
 func _on_button_pressed() -> void:
-	SceneManager.call_scene("world")
+	SceneManager.call_scene("tutorial")
