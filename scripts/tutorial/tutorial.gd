d65f020dcc9195146e8fded2e4e91474b94f3bcf
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-02-13 11:14:46 +0100

    Update tutorial dialog

--- scripts/tutorial/tutorial.gd
+++ scripts/tutorial/tutorial.gd
@@ -11,6 +11,8 @@ func _ready() -> void:
 func _dropped_off(_customer_name: String, _location: Vector3) -> void:
 	var dialog = "Yo, that was amazing! That's how we do it! Maybe you can be a little faster next time and even get a bonus from the customer!"
 	DialogManager.call_dialog("Shiba", dialog)
+	dialog = "Okay, for now that's good though! You can now drive through the tunnel to start your work and go to the main city!"
+	DialogManager.call_dialog("Shiba", dialog)
 	
 func _picked_up(customer_name: String, _location: Vector3) -> void:
 	var dialog = "Woah, " + customer_name + ", that's our first customer? Let's bring them to the place they want to go to! Check the taxi app to find the location."
